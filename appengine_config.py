f2383ea7065bd7fab3f9b09315abb3642b8d958b
Ka-Ping Yee <kpy@google.com>
2013-09-25 18:26:01 -0700

    tpvz for tiles. need to add map support to []
    probably also need to do the MOE:strip dance here, since AFAIK it's not
    open-sourced.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=46031378

---
+++ appengine_config.py
@@ -0,0 +1,18 @@
+# Copyright 2012 Google Inc.  All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.  You may obtain a copy
+# of the License at: http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distrib-
+# uted under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, either express or implied.  See the License for
+# specific language governing permissions and limitations under the License.
+"""Crisismap application configuration.
+
+appengine_config.py is always loaded by the Python 2.7 runtime; it is
+loaded implicitly to retrieve certain configuration values.
+See:
+  https://developers.google.com/appengine/docs/python/tools/appengineconfig
+"""
+
