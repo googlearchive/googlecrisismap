e5c37964de9fd6233b2d26c923e6380419b73a91
Raquel Romano <romano@google.com>
2014-06-10 17:39:56 -0700

    html-css-sanitizer.js exports as window['html'], so we should use that. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=66357362

--- js/sanitizer_module.js
+++ js/sanitizer_module.js
@@ -15,12 +15,12 @@
  */
 goog.require('goog.module');
 
-// This module exports the 'html' symbol from html-sanitizer.js, provided by
-// //third_party/java/caja:html_css_sanitizer.  There is no goog.require(...)
-// here because html-sanitizer.js doesn't have a goog.provide declaration.
+// This module exports the 'html' symbol from html-css-sanitizer.js, which
+// is provided by the Caja project.  There is no goog.require(...) here because
+// html-css-sanitizer.js doesn't have a goog.provide declaration.
 
 // One provide() line for each object to export.
-goog.module.provide('sanitizer', 'html', html);
+goog.module.provide('sanitizer', 'html', window['html']);
 
 // Last, announce that the module has finished loading.
 goog.module.provide('sanitizer');
